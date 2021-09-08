ce96287b7b06d56d81641e62f79352d47000433a
Enzo423 <you@example.com>
2021-09-08 15:32:03 -0300

    Cuarto commit: se agrega la variable  para calcular la altura en decimetros

--- TP3.php
+++ TP3.php
@@ -2,6 +2,7 @@
    $altura = $_GET['altura'];
    $diametro = $_GET['diametro'];
    $radio = $diametro / 2;
-   
+   $alturadm = $altura * 10; // Esta variable solo se utiliza para sacar el volumen  
+
   
 ?>
