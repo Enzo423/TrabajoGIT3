970ae2f187fd8bfe7ded97b0b355a2cd88aa83d8
Enzo423 <you@example.com>
2021-09-08 15:34:31 -0300

    Sexto commit: se agrega la variable  para calcular el volumen en litros (1 litro equivale a 1 decimetro

--- TP3.php
+++ TP3.php
@@ -4,5 +4,5 @@
    $radio = $diametro / 2;
    $alturadm = $altura * 10; // Esta variable solo se utiliza para sacar el volumen  
    $radiodm = $radio * 10; // Esta variable solo se utiliza para sacar el volumen
-  
+   $volumen = 3.14159265359 * ($radiodm * $radiodm) * $alturadm;
 ?>
